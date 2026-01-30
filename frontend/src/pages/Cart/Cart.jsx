12f1317fee881f95b6d31a2c08ce7bbd8349b5ea
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-01-30 11:48:12 +0530

    Added Place Order Page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -2,9 +2,13 @@ import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 import { AiOutlineClose } from "react-icons/ai";
+import {useNavigate} from "react-router-dom"
+
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+
+  const navigate = useNavigate()
   return (
     <div className="cart">
       <div className="cart-items">
@@ -38,8 +42,10 @@ const Cart = () => {
             );
           }
         })}
-      </div>
-      <div className="cart-bottom">
+      </div>{
+
+        getTotalCartAmount()>0 ? (
+        <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
@@ -50,14 +56,14 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
                 <p>Delivery Fee</p>
-                <p>${50}</p>
+                <p>${getTotalCartAmount()===0 ? 0 : 50}</p>
             </div>
             <hr />
             <div className="cart-total-details">
                 <b>Total</b>
-                <b>${getTotalCartAmount() + 50}</b>
+                <b>${getTotalCartAmount()===0?0:getTotalCartAmount() + 50}</b>
             </div>
-            <button>Proceed to checkout</button> 
+            <button onClick={()=>navigate('/order')}>Proceed to checkout</button> 
           </div>
           <div className="cart-promocode">
               <div>
@@ -69,7 +75,15 @@ const Cart = () => {
               </div>
           </div>
         </div>
-      </div>
+      </div>) : (
+  <div className="cart-empty">
+    <h2>Your Cart is Empty</h2>
+    <p>Add some delicious food to get started 🍔</p>
+    <button onClick={() => navigate("/")}>Go to Menu</button>
+  </div>
+)
+      }
+      
     </div>
   );
 };
